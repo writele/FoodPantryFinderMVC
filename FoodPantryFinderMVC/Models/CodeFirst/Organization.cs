44006fb190cfd25882148bdb10e2afd43bf6280e
Ele <ecpalmer21@gmail.com>
2016-11-13 10:37:29 -0500

    Initialized application with .gitignore

---
+++ FoodPantryFinderMVC/Models/CodeFirst/Organization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodPantryFinderMVC.Models
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+        public int AddressId { get; set; }
+        public int ScheduleId { get; set; }
+        public int FrequencyId { get; set; }
+  
+        [Required(ErrorMessage = "Your must provide a PhoneNumber")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
